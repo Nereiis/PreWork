0d9bebdc8e61790b6530f5af9424e90c1a4772f9
Nereiis <123872140+Nereiis@users.noreply.github.com>
2023-03-06 11:22:21 +0100

    Contenido Actualizado

---
+++ JavaScript/ClassJava.js
@@ -0,0 +1,15 @@
+var Sust='perro';
+var Adj='negro';
+var Verb='corrió';
+var Adv='rápidamente';
+
+var PalabrasEnBlanco= 'El '+Sust+' '+Adj+ ' '+Verb+' '+Adv+ ' a la tienda.';
+console.log(PalabrasEnBlanco);
+
+Sust = 'bicicleta';
+Adj = 'pequeña';
+Verb = 'voló';
+Adv = 'lentamente';
+
+var PalabrasEnBlanco = 'La '+Sust+' '+Adj+' '+Verb+' a la tienda '+Adv+'.';
+console.log(PalabrasEnBlanco);
